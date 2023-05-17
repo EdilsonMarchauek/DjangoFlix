1b354064ef89df88a8a1abb4f2a3c5ba146d82d4
EdilsonMarchauek <edilson@luna.ppg.br>
2023-05-17 11:34:55 -0300

    debug para falso

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e&2l4_t)8bpfzhr!z)qdrqa!kc_ze9b7aq+d_ke2b1%a7!oymw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
