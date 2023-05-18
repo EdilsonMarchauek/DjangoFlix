a5f293f20d71b5b5007ccd16f88dad76d135b0f4
EdilsonMarchauek <edilson@luna.ppg.br>
2023-05-18 11:11:23 -0300

    ajustes csrf

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,16 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&2l4_t)8bpfzhr!z)qdrqa!kc_ze9b7aq+d_ke2b1%a7!oymw'
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://projetohashflixdjango-production-30e4.up.railway.app/']
+else:
+    SECRET_KEY = 'django-insecure-e&2l4_t)8bpfzhr!z)qdrqa!kc_ze9b7aq+d_ke2b1%a7!oymw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = ["https://projetohashflixdjango-production-30e4.up.railway.app/", "localhost", "127.0.0.1"]
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -87,7 +91,7 @@ DATABASES = {
 }
 
 import dj_database_url
-import os
+
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
