d01218963cfa502db5a22b9eff00e26926ce8dea
EdilsonMarchauek <edilson@luna.ppg.br>
2023-05-22 09:31:17 -0300

    add super user

--- filme/apps.py
+++ filme/apps.py
@@ -6,17 +6,17 @@ class FilmeConfig(AppConfig):
     name = 'filme'
 
     # criação do superuser
-    # def ready(self):
-    #     from .models import Usuario
-    #     import os
-    #
-    #     email = os.getenv("EMAIL_ADMIN")
-    #     senha = os.getenv("SENHA_ADMIN")
-    #
-    #     usuarios = Usuario.objects.filter(email=email)
-    #     if not usuarios:
-    #         Usuario.objects.create_superuser(username="admin", email=email, password=senha,
-    #                                          is_active=True, is_staff=True)
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha,
+                                             is_active=True, is_staff=True)
 
 
 
